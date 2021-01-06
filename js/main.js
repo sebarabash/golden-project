91dd9f2edcf2075b841bcb32ed9b35e9b979a71c
sebarabash <62248308+sebarabash@users.noreply.github.com>
2021-01-06 23:03:22 +0200

    update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $(".header__burger,.menu__list").toggleClass("active");
     $("body").toggleClass("lock");
   });
+  $(".menu__link").click(function(){
+     $(".header__burger,.menu__list").removeClass("active");
+     $("body").removeClass("lock");
+  });
 });
